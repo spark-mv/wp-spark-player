bce81a232039c87ed139a8481d30fe6813db5dea
Jesse <jesse@hola.org>
2017-02-06 15:03:10 +0200

    corrected handling of boolean flags in shortcode

--- includes/class-hvp-shortcodes.php
+++ includes/class-hvp-shortcodes.php
@@ -29,25 +29,21 @@ class Hvp_Shortcode {
     */
     public function hvp_simple_video($atts, $content) {
         extract(shortcode_atts(array(
-                                        'url' => '',
-                                        'adtagurl'=> '',
-                                        'hls' => false, // Live streaming video
-                                        'osmf' => false,
-                                        'width' => '640',
-                                        'height' => '',
-                                        'controls' => 'true',
-                                        'autoplay' => false,
-                                        'poster' => '',
-                                        'loop' => '',
-                                        'muted' => '',
-                                        'preload' => 'none',
-                                        'ytcontrol' => false, // Youtube control
-                                        'video_id' => 'sample',
-                                        'class' => '',
-                                        'template' => '',
-),
-                                    $atts
-));
+            'url' => '',
+            'adtagurl'=> '',
+            'hls' => false, // Live streaming video
+            'osmf' => false,
+            'width' => '640',
+            'height' => '',
+            'controls' => true,
+            'autoplay' => false,
+            'poster' => '',
+            'loop' => false,
+            'muted' => false,
+            'preload' => 'none',
+            'ytcontrol' => false, // Youtube control
+            'class' => '',
+            'template' => ''), $atts));
         $url = $this->model->hvp_escape_slashes_deep($url);
         
         // Get file MIME type
@@ -101,10 +97,10 @@ class Hvp_Shortcode {
             <?php print $custom_css;?>
             </style>
         <?php            
-        $muted = ($muted == true || $muted == 'true') ? ' muted' : '';
-        $autoplay = ($autoplay == true || $autoplay == 'true') ? ' autoplay' : '';
-        $loop = ($loop == true || $loop == 'true') ? ' loop' : '';
-        $controls = ($controls == 'true') ? ' controls' : '';
+        $muted = ($muted === true || $muted === 'true') ? 'muted' : '';
+        $autoplay = ($autoplay === true || $autoplay === 'true') ? 'autoplay' : '';
+        $loop = ($loop === true || $loop === 'true') ? 'loop' : '';
+        $controls = ($controls === true || $controls === 'true') ? 'controls' : '';
         $techorder = '';
 
         // Check if youtube url added
@@ -114,7 +110,7 @@ class Hvp_Shortcode {
             $techorder = '"techOrder": ["youtube"],'; // Videojs attrib
 
             // Check for display youtube control or not
-            if($ytcontrol == true) {
+            if($ytcontrol === true || $ytcontrol === 'true') {
                 $controls = '';
                 $techorder .= '"youtube": { "ytControls": 2 },';
             }
@@ -125,7 +121,7 @@ class Hvp_Shortcode {
             $techorder = '"techOrder": ["vimeo"],'; // Videojs attrib
 
             // Check for display vimeo control or not
-            if($ytcontrol == true) {
+            if($ytcontrol === true || $ytcontrol === 'true') {
                 $controls = '';
                 $techorder .= '"vimeo": { "ytControls": 2 },';
             }            
@@ -134,12 +130,14 @@ class Hvp_Shortcode {
 
         ob_start();
         ?>
-        <div id="<?php print $video_id;?>" class="hvp-video hvp-content-video">
-          <video id="<?php print $res_class?>" <?php print $adtagurl ?> data-id="<?php print $res_class?>" class="video-js <?php print $skin.' '.$res_class ?> <?php print $class?>" preload="<?php print $preload; ?>" width="<?php print $width?>" height="<?php print $height?>" poster="<?php print $poster;?>" <?php print $autoplay.$muted.$loop.$controls ?>  data-setup='{<?php print $techorder ?>"plugins":{}}'>
-            <source src="<?php print $url?>" type="<?php print $mime_type?>" />
-            <p class="vjs-no-js"><?php _e('To view this video please enable JavaScript, and consider upgrading to a web browser that', HVP_TEXTDOMAIN) ?> <a href="http://videojs.com/html5-video-support/" target="_blank"><?php _e('supports HTML5 video', HVP_TEXTDOMAIN) ?></a></p>
-              </video>
-          </div>
+        <div class="hvp-video hvp-content-video">
+          <video id="<?php print $res_class?>" <?php print $adtagurl ?> data-id="<?php print $res_class?>" class="video-js <?php print $skin.' '.$res_class ?> <?php print $class?>" 
+            preload="<?php print $preload; ?>" width="<?php print $width?>" height="<?php print $height?>" poster="<?php print $poster;?>" 
+            <?php print "$autoplay $muted $loop $controls"; ?>  data-setup='{<?php print $techorder ?>"plugins":{}}'>
+                <source src="<?php print $url?>" type="<?php print $mime_type?>" />
+                <p class="vjs-no-js"><?php _e('To view this video please enable JavaScript, and consider upgrading to a web browser that', HVP_TEXTDOMAIN) ?> <a href="http://videojs.com/html5-video-support/" target="_blank"><?php _e('supports HTML5 video', HVP_TEXTDOMAIN) ?></a></p>
+          </video>
+        </div>
         <?php
         $content .= ob_get_clean();
         return $content;
@@ -155,5 +153,4 @@ class Hvp_Shortcode {
         // Shortcode for showing video
         add_shortcode('hvp-video', array($this, 'hvp_simple_video'));
     }
-
 }
