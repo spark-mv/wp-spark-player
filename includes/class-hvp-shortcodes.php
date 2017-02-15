6a4fd27f9676217a73df9b96491b0fe2b39e8897
Jesse <jesse@hola.org>
2017-02-15 14:41:18 +0200

    don't use auto-setup video.js for YT or Vimeo

--- includes/class-hvp-shortcodes.php
+++ includes/class-hvp-shortcodes.php
@@ -86,30 +86,35 @@ class Hvp_Shortcode {
         $autoplay = ($autoplay === true || $autoplay === 'true') ? 'autoplay' : '';
         $loop = ($loop === true || $loop === 'true') ? 'loop' : '';
         $controls = ($controls === true || $controls === 'true') ? 'controls' : '';
-        $techorder = '';
+
+        // Causes auto-setup of videojs. Can't use with YT, Vimeo, or ads
+        $data_setup = 'data-setup="{plugins:{}}"';
 
         // Check if youtube url added
         if($mime_type == 'video/youtube') {
             // Include youtube support js
             wp_enqueue_script('hvp_youtube_video_script');
             $techorder = '"techOrder": ["youtube"],'; // Videojs attrib
+            $data_setup = '';
 
             // Check for display youtube control or not
             if($ytcontrol === true || $ytcontrol === 'true') {
                 $controls = '';
                 $techorder .= '"youtube": { "ytControls": 2 },';
             }
-
+            wp_add_inline_script('hvp_youtube_video_script', "videojs('$res_class', { $techorder });");
         } elseif ($mime_type == 'video/vimeo') {
             // Include vimeo support js
             wp_enqueue_script('hvp_vimeo_video_script');
             $techorder = '"techOrder": ["vimeo"],'; // Videojs attrib
+            $data_setup = '';
 
             // Check for display vimeo control or not
             if($ytcontrol === true || $ytcontrol === 'true') {
                 $controls = '';
                 $techorder .= '"vimeo": { "ytControls": 2 },';
             }            
+            wp_add_inline_script('hvp_vimeo_video_script', "videojs('$res_class', { $techorder });");
         }
 
         $adtagurl = (!empty($adtagurl)) ? 'data-adurl="'. $adtagurl .'" ' : '';
@@ -128,15 +133,16 @@ class Hvp_Shortcode {
 
             // ADS init script
             wp_enqueue_script('hvp_public_ads_script');
-        }
 
+            $data_setup = '';
+        }
 
         ob_start();
         ?>
         <div class="hvp-video hvp-content-video">
           <video id="<?php print $res_class?>" <?php print $adtagurl ?> data-id="<?php print $res_class?>" class="video-js <?php print $skin.' '.$res_class ?> <?php print $class?>" 
             preload="<?php print $preload; ?>" width="<?php print $width?>" height="<?php print $height?>" poster="<?php print $poster;?>" 
-            <?php print "$autoplay $muted $loop $controls"; ?>  data-setup='{<?php print $techorder ?>"plugins":{}}'>
+            <?php print "$autoplay $muted $loop $controls $data_setup"; ?>>
                 <source src="<?php print $url?>" type="<?php print $mime_type?>" />
                 <p class="vjs-no-js"><?php _e('To view this video please enable JavaScript, and consider upgrading to a web browser that', HVP_TEXTDOMAIN) ?> <a href="http://videojs.com/html5-video-support/" target="_blank"><?php _e('supports HTML5 video', HVP_TEXTDOMAIN) ?></a></p>
           </video>
